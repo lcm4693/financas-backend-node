eb783402d99987cc5cb61b2f8e19c60d5b005f36
Diego Serpa <diegos.java@gmail.com>
2020-02-13 20:09:48 -0300

    adicionada validação se a despesa já existe

--- src/service/despesa.service.ts
+++ src/service/despesa.service.ts
@@ -1,5 +1,5 @@
-import { Despesa } from '../entity/despesa';
-import { Injectable } from '@nestjs/common';
+import { Despesa } from './../entity/despesa';
+import { Injectable, HttpException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -13,7 +13,20 @@ export class DespesaService {
     return this.despesaModel.find().exec();
   }
 
+  async verificarExistenciaObjeto(despesa: Despesa): Promise<boolean> {
+    const objeto = await this.despesaModel.findOne({ id: despesa.id }).exec();
+    if (objeto) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   async save(despesaParam: Despesa): Promise<Despesa> {
+    const objetoJaExiste = await this.verificarExistenciaObjeto(despesaParam);
+    if (objetoJaExiste === true) {
+      throw new HttpException('Esse cara ja existe', 403);
+    }
     const despesaManaged = new this.despesaModel(despesaParam);
     return despesaManaged.save();
   }
